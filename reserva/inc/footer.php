115dee214c1287aacdaf802a32e6294eb0ca26ba
DreamZoneKAJ <jetsaminbellogonzalez@gmail.com>
2024-06-07 21:10:05 -0600

    Add files via upload

---
+++ reserva/inc/footer.php
@@ -0,0 +1,6 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">Dream Zone &copy; 2024</h5>
+</footer>
